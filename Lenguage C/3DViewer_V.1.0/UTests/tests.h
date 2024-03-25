d73202b370aa4c0b040ad35f2f6ff68f8791dc6b
PeachJuice <75912368+Anna-good@users.noreply.github.com>
2024-03-26 00:24:37 +0300

    Create tests.h

---
+++ Lenguage C/3DViewer_V.1.0/UTests/tests.h
@@ -0,0 +1,11 @@
+#ifndef SRC_UTESTS_TESTS_H_
+#define SRC_UTESTS_TESTS_H_
+
+#include <check.h>
+
+#include "../3DViewerV1.0/back/s21_3d_viewer.h"
+
+Suite *test_obj_file(void);
+Suite *test_affine(void);
+
+#endif // SRC_UTESTS_TESTS_H_
